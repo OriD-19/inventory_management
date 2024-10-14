a7a70243949b46cc144f61212b4427e4708d6976
Fer Fuentes <fernandocastillo.8@hotmail.com>
2024-10-14 15:32:59 -0600

    SQUASHME: still dumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 from inventory_app.app import create_app
+import sqlalchemy_libsql
 
 load_dotenv()
 
@@ -9,4 +10,4 @@ def app(environ, start_response):
     return flask_app.wsgi_app(environ=environ, start_response=start_response)
 
 if __name__ == '__main__':
-    app()
+    flask_app.run()

62df15b68fb1f010ca186d2915f2cd3b80ae2646
Fer Fuentes <fernandocastillo.8@hotmail.com>
2024-10-14 16:11:54 -0600

    SQUASHME: i'm trying, ok?

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from dotenv import load_dotenv
 from inventory_app.app import create_app
 import sqlalchemy_libsql
+import os
 
 load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=os.getenv("PORT"), debug=True)

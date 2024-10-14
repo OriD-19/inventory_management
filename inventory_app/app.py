b1b611b76fa707190a9714769627cfe855ed7711
Fer Fuentes <fernandocastillo.8@hotmail.com>
2024-10-14 17:20:04 -0600

    fix: maybe this works

--- inventory_app/app.py
+++ inventory_app/app.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='templates')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite+{os.getenv('TURSO_DATABASE_URL')}?authToken={os.getenv('TURSO_DATABASE_AUTH_TOKEN')}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///trial.db"
 
     db.init_app(app)
 
@@ -16,5 +16,8 @@ def create_app():
     app.register_blueprint(trial, url_prefix='/trial')
 
     migrate = Migrate(app, db)
+
+    with app.app_context():
+        db.create_all()
 
     return app
